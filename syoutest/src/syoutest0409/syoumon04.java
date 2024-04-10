815f1fc71cc8b881842cc0e8f519c6740b718c45
dir-FY24hiraiyuta <yuta.hirai.f410@dirbato.co.jp>
2024-04-10 16:16:05 +0900

    最初のコミット

---
+++ syoutest/src/syoutest0409/syoumon04.java
@@ -0,0 +1,14 @@
+package syoutest0409;
+
+public class syoumon04 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i = i - 1;
+        }
+
+    }
+
+}
---
+++ syoutest/src/syoutest0409/syoumon08.java
@@ -0,0 +1,16 @@
+package syoutest0409;
+
+public class syoumon08 {
+
+    public static void main(String[] args) {
+        int a = 5, b = 5, c = 5;
+        if ( a == b && b == c ) {
+            System.out.println("正三角形");
+        } else {
+            System.out.println("正三角形ではない");
+        }
+
+
+    }
+
+}
